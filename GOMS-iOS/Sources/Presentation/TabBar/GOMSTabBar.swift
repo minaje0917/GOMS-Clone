a75623755ff98ca5021a6a6b7f57fcde1fa75e4b
minaje0917 <k050918@naver.com>
2023-09-21 09:57:36 +0900

    :lipstick: UI :: Change Color

--- GOMS-iOS/Sources/Presentation/TabBar/GOMSTabBar.swift
+++ GOMS-iOS/Sources/Presentation/TabBar/GOMSTabBar.swift
@@ -8,8 +8,6 @@
 import UIKit
 
 final class GOMSTabBarViewController: UITabBarController {
-    private let keychain = Keychain()
-    private lazy var userAuthority = keychain.read(key: Const.KeychainKey.authority)
     
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -23,7 +21,7 @@ final class GOMSTabBarViewController: UITabBarController {
 
 private extension GOMSTabBarViewController {
     func configureVC() {
-        tabBar.tintColor = userAuthority == "ROLE_STUDENT_COUNCIL" ? UIColor.adminColor : UIColor.mainColor
+        tabBar.tintColor = UIColor.mainColor
         tabBar.unselectedItemTintColor = UIColor.subColor
         tabBar.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.98)
     }
